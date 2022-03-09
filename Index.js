ec5bbb6007b60369b901ca36ae039f90282eb43d
Joseph Sasmito <josephsasmito11@gmail.com>
2022-03-09 16:57:41 +0700

    Enchanting Skill Display Support, missing the icon tho

--- Index.js
+++ Index.js
@@ -185,9 +185,62 @@ App.post("/", async (req, res) => {
         //Setting hypermaxed settings;
         if (miningData.level >= 50) miningData.hypermaxed = true;
 
+        //Enchanting Skill
+
+        const EnchantingXPGoals = [0, 50, 175, 375, 675, 1175, 1925, 2925, 4425, 6425, 9925, 14925, 22425,
+            32425, 47425, 67425, 97425, 147425, 222425, 322425, 522425, 822425, 1222425, 1722425, 2322425,
+            3022425, 3822425, 4722425, 5722425, 6822425, 8022425, 9322425, 10722425, 12222425, 13822425,
+            15522425, 17322425, 19222425, 21222425, 23322425, 25522425, 27822425, 30222425, 32722425,
+            35322425, 38072425, 40972425, 44072425, 47472425, 51172425, 55172425, 59472425, 64072425,
+            68972425, 74172425, 79672425, 85472425, 91572425, 97972425, 104672425, 111672425];
+        let enchantingData = {
+            xp: null,
+            level: null,
+            abbrev: null,
+            skill_progression_percentage: null,
+            hypermaxed: false,
+            greyPercentage: null,
+            nextLevelXP: null
+        };
+
+        enchantingData.xp = SkySimData.data.enchantXP;
+
+        EnchantingXPGoals.forEach((enchantingXP) => {
+            if ((SkySimData.data.enchantXP - enchantingXP) >= 1) enchantingData.level = EnchantingXPGoals.findIndex((xp) => xp === enchantingXP);
+        });
+
+        //Changing XP Format;
+        enchantingData.abbrev = abbreviateNumber(enchantingData.xp);
+
+        //Setting the next level xp;
+        const enchantingNextXP = enchantingData.level === 60 ? 'maxed' : enchantingData.level === 59 ? EnchantingXPGoals[enchantingData.level + 1] : EnchantingXPGoals[enchantingData.level + 1];
+
+        enchantingData.nextLevelXP = enchantingNextXP == "maxed" ? 'maxed' : abbreviateNumber(enchantingNextXP);
+
+        //Calculating progress bar percentage.
+        let raw_data_2 = enchantingNextXP == "maxed" ? 100 : SkySimData.data.enchantXP / EnchantingXPGoals[enchantingData.level + 1] * 100;
+
+        if (raw_data_2 >= 100) raw_data_2 = 100;
+        else if (raw_data_2 >= 1) {
+            raw_data_2 = raw_data_2;
+        };
+
+        //Getting 30% of the bar percentage above;
+
+        const div1_2 = 30 / 100;
+
+        const percent_of_percentage_2 = div1_2 * raw_data_2;
+
+        enchantingData.skill_progression_percentage = percent_of_percentage_2;
+
+        enchantingData.greyPercentage = 30 - percent_of_percentage_2;
+
+        //Setting hypermaxed settings;
+        if (enchantingData.level >= 50) enchantingData.hypermaxed = true;
+
         //Rendering page.
 
-        console.log(combData);
+        console.log(enchantingData, SkySimData.data);
 
         res.render('stats', {
             data: SkySimData.data,
@@ -195,7 +248,8 @@ App.post("/", async (req, res) => {
             uuidData: UUID.data?.data?.player,
             skills: {
                 combat: combData,
-                mining: miningData
+                mining: miningData,
+                enchanting: enchantingData
             }
         });
     };
